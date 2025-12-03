3e561b9063d451a7a68df731fe9e9efc311c6a09
Kevin Lim <kevinlim2030@gmail.com>
2025-12-03 15:50:04 +0100

    Update ft_printf.h

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdarg.h>
