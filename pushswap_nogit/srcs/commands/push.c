5d9df9176b63a3810281dc450974481ff748832b
Kevin Lim <kevinlim2030@gmail.com>
2026-01-06 17:59:56 +0100

    Push_swap (not finished)

---
+++ pushswap_nogit/srcs/commands/push.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevlim <kevlim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/06 17:29:14 by kevlim            #+#    #+#             */
+/*   Updated: 2026/01/06 17:30:31 by kevlim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/push_swap.h"
+
+static void	push(t_stack_node **dst, t_stack_node **src)
+{
+	t_stack_node	*push_node;
+
+	if (!*src)
+		return ;
+	push_node = *src;
+	*src = (*src)->next;
+	if (*src)
+		(*src)->prev = NULL;
+	push_node->prev = NULL;
+	if (!*dst)
+	{
+		*dst = push_node;
+		push_node->next = NULL;
+	}
+	else
+	{
+		push_node->next = *dst;
+		push_node->next->prev = push_node;
+		*dst = push_node;
+	}
+}
+
+void	pa(t_stack_node **a, t_stack_node **b, bool print)
+{
+	push(a, b);
+	if (!print)
+		ft_printf("pa\n");
+}
+
+void	pb(t_stack_node **b, t_stack_node **a, bool print)
+{
+	push(b, a);
+	if (!print)
+		ft_printf("pb\n");
+}
